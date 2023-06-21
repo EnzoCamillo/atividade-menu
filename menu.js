50d091667037085b1dbdf11ea951914a29dc9f1a
Enzo <enzocamillo456@gmail.com>
2023-06-21 16:56:05 -0300

    Atividade Menu

--- menu.js
+++ menu.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-    var navbar = document.getElementById('navbar');
-    var sticky = navbar.offsetTop;
-    winsow.onscroll = function() { myFunction(navbar, sticky) };
+    var navbar = document.getElementById("navbar")
+    var sticky = navbar.offsetTop
+    window.onscroll = function() { myFunction (navbar, sticky) }
 }
 
 function myFunction(navbar, sticky){
     if(window.pageYOffset >= sticky){
-        navbar.classList.add('sticky');
-    } else{
-        navbar.classList.remove('sticky');
+        navbar.classList.add("sticky")
+    }else{
+        navbar.classList.remove("sticky")
     }
 }
